7c0bfdfe21eab42cdcec5d2786fa035964a799ad
Serhii Mikushev <sergemi.kh@gmail.com>
2024-10-02 11:31:21 +0100

    Initial Commit

---
+++ Matchermind/MatchermindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchermindApp.swift
+//  Matchermind
+//
+//  Created by sergemi on 02.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MatchermindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

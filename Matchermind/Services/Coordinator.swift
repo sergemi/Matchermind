2c1162edd4c972f3c496e6e0672591f48ce52204
Serhii Mikushev <sergemi.kh@gmail.com>
2024-10-07 16:47:36 +0100

    first scratch

--- Matchermind/Services/Coordinator.swift
+++ Matchermind/Services/Coordinator.swift
@@ -7,4 +7,32 @@
 
 import SwiftUI
 
+//@Observable class Coordinator {
+//    var learnNavigationPath = NavigationPath()
+class Coordinator: ObservableObject {
+    @State var learnNavigationPath = NavigationPath()
 
+    
+    @ViewBuilder func learnFlow() -> some View {
+        NavigationStack(path: $learnNavigationPath) {
+            MainLearnView()
+        }
+        .navigationDestination(for: NavLink.self, destination: linkDestination)
+    }
+    
+    @ViewBuilder private func linkDestination(link: NavLink) -> some View {
+        switch link {
+        case .mainLearnView:
+            MainLearnView()
+            
+        case .resumeLearnView:
+            ResumeLearnView()
+            
+        case .startNewLearnView:
+            StartNewLearnView()
+            
+        default:
+            EmptyView()
+        }
+    }
+}

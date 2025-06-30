7c4dfec61f75bd9c87e09e866bb0bf1f80d44b8e
Serhii Mikushev <sergemi.kh@gmail.com>
2025-06-30 23:53:46 +0100

    update PreviewWrapper

--- Matchermind/Helpers/PreviewWrapper.swift
+++ Matchermind/Helpers/PreviewWrapper.swift
@@ -7,10 +7,11 @@
 
 import SwiftUI
 
+@MainActor
 struct PreviewWrapper<Content: View>: View {
     // Здесь мы создаем источник истины для превью
-    @State private var router = AppRouter()
-    @State private var errorManager = ErrorManager()
+    @State private var router: AppRouter
+    @State private var errorManager: ErrorManager
     @State private var authService: AuthService
     @State private var dataManager: DataManager
 
@@ -24,6 +25,9 @@ struct PreviewWrapper<Content: View>: View {
         @ViewBuilder content: @escaping () -> Content
     ) {
         // Важно: Инициализируем @State переменные именно так в init
+        self._router = State(initialValue: AppRouter())
+        self._errorManager = State(initialValue: ErrorManager())
+        
         self._authService = State(initialValue: AuthService(service: MockAuthService.initWithMockUser(loginned: loginned)))
         self._dataManager = State(initialValue: MocDataManager(testDelayMax: testDelayMax, withData: withData))
         self.content = content

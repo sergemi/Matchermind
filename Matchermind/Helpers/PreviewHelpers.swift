e8839bc231724ceed2833a810a77c14616364704
Serhii Mikushev <sergemi.kh@gmail.com>
2025-06-30 22:59:33 +0100

    refactor PreviewHelpers

--- Matchermind/Helpers/PreviewHelpers.swift
+++ Matchermind/Helpers/PreviewHelpers.swift
@@ -29,13 +29,15 @@ func withMockEnvironment<Content: View>(
 @MainActor @ViewBuilder
 func withMockDataEnvironment<Content: View>(
     loginned: Bool = true,
+    testDelayMax: Int = 0,
+    withData: Bool = true,
     @ViewBuilder content: @escaping () -> Content
 ) -> some View {
     let mocRouter = AppRouter()
     let errorMgr = ErrorManager()
     let mockService = MockAuthService.initWithMockUser(loginned: loginned)
     let mockWrapper = AuthService(service: mockService)
-    let mocDataMgr: DataManager = MocDataManager()
+    let mocDataMgr: DataManager = MocDataManager(testDelayMax: testDelayMax, withData: withData)
 
     content()
         .environment(mocRouter)

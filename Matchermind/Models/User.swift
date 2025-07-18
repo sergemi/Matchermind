f71a0ccfc6420cdf59488bf6e653a7bd3327db98
Serhii Mikushev <sergemi.kh@gmail.com>
2025-07-18 13:52:59 +0100

    add Codable to User model

--- Matchermind/Models/User.swift
+++ Matchermind/Models/User.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct User: Equatable, Identifiable {
+struct User: Equatable, Identifiable, Codable {
     let id: String
     let email: String
     var name: String? = nil //TODO: maybe change inicialization later
